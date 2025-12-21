1d1540189a3488dea05579956a43471b6d985941
David Janssens <fiammybe@gmail.com>
2025-12-21 11:01:38 +0100

    add Dutch translations

---
+++ src/language/nederlands/main.php
@@ -0,0 +1,28 @@
+<?php
+// Frontend strings
+
+define('_MD_SIMPLECART_SHOP_TITLE', 'Producten');
+define('_MD_SIMPLECART_ADD_TO_CART', 'Plaats in winkelmandje');
+define('_MD_SIMPLECART_YOUR_CART', 'Winkelmandje');
+define('_MD_SIMPLECART_REMOVE', 'Verwijder');
+define('_MD_SIMPLECART_TOTAL', 'Totaal');
+define('_MD_SIMPLECART_CHECKOUT', 'Checkout');
+define('_MD_SIMPLECART_EMPTY_CART', 'Het winkelmandje is leeg');
+
+define('_MD_SIMPLECART_CSRF_FAIL', 'Security check gefaald. Herlaad de pagina en probeer het opnieuw.');
+define('_MD_SIMPLECART_ORDER_CREATE_FAIL', 'De bestelling kon niet aangemaakt worden');
+define('_MD_SIMPLECART_ORDERITEM_CREATE_FAIL', 'Een onderdeel van de bestelling kon niet worden toegevoegd');
+
+define('_MD_SIMPLECART_ORDER_CONFIRMED', 'Bestelling ontvangen');
+define('_MD_SIMPLECART_ORDER_THANKS', 'Dank u voor uw bestelling!');
+define('_MD_SIMPLECART_ORDER_SUCCESS', 'De bestelling werd met succes geplaatst');
+
+define('_MD_SIMPLECART_NAME', 'Naam');
+define('_MD_SIMPLECART_EMAIL', 'Email');
+define('_MD_SIMPLECART_PHONE', 'Telefoon');
+define('_MD_SIMPLECART_ADDRESS', 'Adres');
+define('_MD_SIMPLECART_PLACE_ORDER', 'Plaats bestelling');
+
+define('_MD_SIMPLECART_PAY_WITH_SEPA', 'Betaal met overschrijving QR Code');
+define('_MD_SIMPLECART_SCAN_TO_PAY', 'Scan deze code met uw bankapp om te betalen via overschrijving.');
+?>
