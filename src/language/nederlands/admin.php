1c2b6423cfd146576a89386434ef3a1c68c1c2a2
David Janssens <fiammybe@gmail.com>
2026-01-04 22:57:24 +0100

    add NL language

---
+++ src/language/nederlands/admin.php
@@ -0,0 +1,15 @@
+<?php
+// Admin language
+
+define('_AM_SIMPLECART_PRODUCT_ADMIN', 'Beheer producten');
+define('_AM_SIMPLECART_ORDER_ADMIN', 'Bestellingen');
+
+define('_AM_SIMPLECART_PRODUCT_FORM', 'Product');
+define('_AM_SIMPLECART_PRODUCT_CREATE', 'Product Aanmaken');
+define('_AM_SIMPLECART_PRODUCT_CREATED', 'Product met succes aangemaakt');
+define('_AM_SIMPLECART_PRODUCT_UPDATED', 'Product met succes gewijzigd');
+define('_AM_SIMPLECART_PRODUCT_DELETE_CONFIRM', 'Ben je zeker dat je dit product wil verwijderen?');
+
+define('_AM_SIMPLECART_ORDER_VIEW', 'Bestelling');
+define('_AM_SIMPLECART_ORDER_NOT_FOUND', 'Bestelling niet gevonden');
+
