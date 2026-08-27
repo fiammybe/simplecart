ef9add85e9c5053ccb01c63be7ee98840471685b
fiammybe <david.j@impresscms.org>
2026-08-27 22:47:25 +0200

    add admin check to all admin pages on top of cp_admin.php verification

--- src/admin/header.php
+++ src/admin/header.php
@@ -2,6 +2,11 @@
 include_once __DIR__ . '/../../../include/cp_header.php';
 include_once __DIR__ . '/../include/common.php';
 
+if (!icms::$user || !icms::$user->isAdmin()) {
+    redirect_header(ICMS_URL, 3, 'Access denied');
+    exit;
+}
+
 if (!is_object(icms::$module) || icms::$module->getVar('dirname') !== 'simplecart') {
     $module_handler = icms::handler('icms_module');
     $icmsModule = $module_handler->getByDirname('simplecart');
